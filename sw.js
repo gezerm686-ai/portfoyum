7cf05ebba9e0c2516e18439039f88a25a810bd4f
gezerm686-ai <gezerm686@gmail.com>
2026-09-07 22:44:19 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const C='portfoyum-v6';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll([
+ './','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./icons/favicon-32.png'
+]))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
